07ae55e09220b16eaf7d6cd835e65477758e1300
olyakolesnichenko <kolesnichenko.zp@gmail.com>
2019-03-01 22:12:56 +0200

    add tests for fetchUsers saga

--- source/bus/ui/__tests__/reducer.js
+++ source/bus/ui/__tests__/reducer.js
@@ -5,46 +5,46 @@ import { uiReducer } from "../reducer";
 import { uiActions } from "../actions";
 
 describe("ui reducer: ", () => {
-  test("should return initial state by default", () => {
-    expect(uiReducer(void 0, {})).toMatchInlineSnapshot(`
+    test("should return initial state by default", () => {
+        expect(uiReducer(void 0, {})).toMatchInlineSnapshot(`
 Immutable.Map {
   "isFetching": false,
   "isOnline": false,
 }
 `);
-  });
-  test("should handle START_FETCHING action", () => {
-    expect(uiReducer(void 0, uiActions.startFetching())).toMatchInlineSnapshot(`
+    });
+    test("should handle START_FETCHING action", () => {
+        expect(uiReducer(void 0, uiActions.startFetching())).toMatchInlineSnapshot(`
 Immutable.Map {
   "isFetching": true,
   "isOnline": false,
 }
 `);
-  });
-  test("should handle STOP_FETCHING action", () => {
-    expect(uiReducer(void 0, uiActions.stopFetching())).toMatchInlineSnapshot(`
+    });
+    test("should handle STOP_FETCHING action", () => {
+        expect(uiReducer(void 0, uiActions.stopFetching())).toMatchInlineSnapshot(`
 Immutable.Map {
   "isFetching": false,
   "isOnline": false,
 }
 `);
-  });
-  test("should handle SET_ONLINE_STATE action", () => {
-    expect(uiReducer(void 0, uiActions.setOnlineState()))
-      .toMatchInlineSnapshot(`
+    });
+    test("should handle SET_ONLINE_STATE action", () => {
+        expect(uiReducer(void 0, uiActions.setOnlineState()))
+            .toMatchInlineSnapshot(`
 Immutable.Map {
   "isFetching": false,
   "isOnline": true,
 }
 `);
-  });
-  test("should handle SET_OFFLINE_STATE action", () => {
-    expect(uiReducer(void 0, uiActions.setOfflineState()))
-      .toMatchInlineSnapshot(`
+    });
+    test("should handle SET_OFFLINE_STATE action", () => {
+        expect(uiReducer(void 0, uiActions.setOfflineState()))
+            .toMatchInlineSnapshot(`
 Immutable.Map {
   "isFetching": false,
   "isOnline": false,
 }
 `);
-  });
+    });
 });
