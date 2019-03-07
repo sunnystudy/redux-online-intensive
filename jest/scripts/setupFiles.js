f685b59914ae7e7f69d7a074bdc2e2578566cc04
olyakolesnichenko <kolesnichenko.zp@gmail.com>
2019-03-08 01:51:16 +0200

    add test with expectSaga

--- jest/scripts/setupFiles.js
+++ jest/scripts/setupFiles.js
@@ -71,6 +71,10 @@ const fetchResponseSuccess = {
     json:   jest.fn(() => Promise.resolve(responseDataSuccess)),
 };
 
+const fetchResponseSuccess204 = {
+    status: 204,
+};
+
 const fetchResponseFail401 = {
     status: 401,
     json:   jest.fn(() => Promise.resolve(responseDataFail)),
@@ -93,6 +97,7 @@ global.__ = {
     token,
     error,
     responseDataSuccess,
+    fetchResponseSuccess204,
     responseDataFail,
     fetchResponseSuccess,
     fetchResponseFail401,
--- source/bus/users/__tests__/fetchUsers.saga.js
+++ source/bus/users/__tests__/fetchUsers.saga.js
@@ -14,74 +14,74 @@ const saga = cloneableGenerator(fetchUsers)(fetchUsersAction);
 let clone = null;
 
 describe("fetchUsers saga: ", () => {
-    describe("should pass until response recived: ", () => {
-        test('should dispatch "startFetching" action', () => {
-            expect(saga.next().value).toEqual(put(uiActions.startFetching()));
-        });
-        test("should call a fetch request", () => {
-            expect(saga.next().value).toEqual(apply(api, api.users.fetch));
-            clone = saga.clone();
-        });
+  describe("should pass until response recived: ", () => {
+    test('should dispatch "startFetching" action', () => {
+      expect(saga.next().value).toEqual(put(uiActions.startFetching()));
     });
-    describe("should handle a 400 status response: ", () => {
-        test("a fetch request should return 400 status response", () => {
-            expect(clone.next(__.fetchResponseFail400).value).toEqual(
-                apply(__.fetchResponseFail400, __.fetchResponseFail400.json)
-            );
-        });
-        test("should contain a response data object", () => {
-            expect(clone.next(__.responseDataFail).value).toEqual(
-                put(uiActions.emitError(__.error, "fetchUsers worker"))
-            );
-        });
-        test('should dispatch "stopFetching" action', () => {
-            expect(clone.next().value).toEqual(put(uiActions.stopFetching()));
-        });
-        test("should finish", () => {
-            expect(clone.next().done).toBe(true);
-        });
+    test("should call a fetch request", () => {
+      expect(saga.next().value).toEqual(apply(api, api.users.fetch));
+      clone = saga.clone();
     });
-    describe("should handle a 200 status response: ", () => {
-        test("a fetch request should return 200 status response data object", () => {
-          expect(saga.next(__.fetchResponseSuccess).value).toEqual(
-            apply(__.fetchResponseSuccess, __.fetchResponseSuccess.json)
-          );
-        });
-        test('should dispatch "fillUsers" action', () => {
-          expect(saga.next(__.responseDataSuccess).value).toMatchInlineSnapshot(`
-    Object {
-      "@@redux-saga/IO": true,
-      "PUT": Object {
-        "action": Object {
-          "payload": Object {
-            "avatar": "TEST_AVATAR",
-            "firstName": "Walter",
-            "id": "TEST_ID",
-            "lastName": "White",
-            "token": "TEST_TOKEN",
-          },
-          "type": "FILL_USERS",
-        },
-        "channel": null,
+  });
+  describe("should handle a 400 status response: ", () => {
+    test("a fetch request should return 400 status response", () => {
+      expect(clone.next(__.fetchResponseFail400).value).toEqual(
+        apply(__.fetchResponseFail400, __.fetchResponseFail400.json)
+      );
+    });
+    test("should contain a response data object", () => {
+      expect(clone.next(__.responseDataFail).value).toEqual(
+        put(uiActions.emitError(__.error, "fetchUsers worker"))
+      );
+    });
+    test('should dispatch "stopFetching" action', () => {
+      expect(clone.next().value).toEqual(put(uiActions.stopFetching()));
+    });
+    test("should finish", () => {
+      expect(clone.next().done).toBe(true);
+    });
+  });
+  describe("should handle a 200 status response: ", () => {
+    test("a fetch request should return 200 status response data object", () => {
+      expect(saga.next(__.fetchResponseSuccess).value).toEqual(
+        apply(__.fetchResponseSuccess, __.fetchResponseSuccess.json)
+      );
+    });
+    test('should dispatch "fillUsers" action', () => {
+      expect(saga.next(__.responseDataSuccess).value).toMatchInlineSnapshot(`
+Object {
+  "@@redux-saga/IO": true,
+  "PUT": Object {
+    "action": Object {
+      "payload": Object {
+        "avatar": "TEST_AVATAR",
+        "firstName": "Walter",
+        "id": "TEST_ID",
+        "lastName": "White",
+        "token": "TEST_TOKEN",
       },
-    }
-    `);
-        });
-        test('should dispatch "stopFetching" action', () => {
-          expect(saga.next().value).toMatchInlineSnapshot(`
-    Object {
-      "@@redux-saga/IO": true,
-      "PUT": Object {
-        "action": Object {
-          "type": "STOP_FETCHING",
-        },
-        "channel": null,
-      },
-    }
-    `);
-        });
-        test("should finish", () => {
-        expect(saga.next().done).toBe(true);
-      });
+      "type": "FILL_USERS",
+    },
+    "channel": null,
+  },
+}
+`);
     });
+    test('should dispatch "stopFetching" action', () => {
+      expect(saga.next().value).toMatchInlineSnapshot(`
+Object {
+  "@@redux-saga/IO": true,
+  "PUT": Object {
+    "action": Object {
+      "type": "STOP_FETCHING",
+    },
+    "channel": null,
+  },
+}
+`);
+    });
+    test("should finish", () => {
+      expect(saga.next().done).toBe(true);
+    });
+  });
 });
